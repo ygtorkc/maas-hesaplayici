7e5e904c73f4c3d02c92e831c9dcc3fdc38a04b5
ygtorkc <109255432+ygtorkc@users.noreply.github.com>
2023-02-12 14:23:45 +0300

    Add files via upload

---
+++ src/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+
+        Employee Yiğit= new Employee("Yiğit Orakçı",1000,55,12) ;
+        System.out.println(Yiğit.toString());
+    }
+}
